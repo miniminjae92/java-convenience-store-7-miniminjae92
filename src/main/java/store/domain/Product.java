699333a7cce884505da45cf8c8a23ef5ba660fd8
minjae k <rkdalswoals@gmail.com>
2024-11-08 15:20:35 +0900

    feat: 상품 클래스 상태 검증 로직 추가

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -2,10 +2,12 @@ package store.domain;
 
 public class Product {
 
-    private String name;
-    private int price;
+    private final String name;
+    private final int price;
 
     public Product(String name, int price) {
+        validateName(name);
+        validatePrice(price);
         this.name = name;
         this.price = price;
     }
@@ -16,5 +18,17 @@ public class Product {
 
     public int getPrice() {
         return price;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 상품명은 빈 값일 수 없습니다.");
+        }
+    }
+
+    private void validatePrice(int price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 0보다 커야 합니다.");
+        }
     }
 }
